19ff4e8630ab9bcd424d9fd3b8c321989fdfa4e1
harrison <harrival56@gmail.com>
2021-09-15 10:40:42 -0700

    trying

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    'DATABASE_URL', 'postgres://:5433/flask-feedback')
+    'DATABASE_URL', 'postgres://:5433/flask-feedback').replace("://", "ql://", 1)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = "harrison"
